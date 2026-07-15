bf45577c9bcc66fb4b08520a1a199fcad84ffe2e
infoavikdatta-git <infoavikdatta@gmail.com>
2026-07-15 21:58:43 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const GROQ_API_KEY = "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";
+const GROQ_API_KEY = "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";
 const GROQ_MODEL = "llama-3.3-70b-versatile";
